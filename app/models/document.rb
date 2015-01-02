ffeed05e4161677f40c877b194996646b4aaaa04
Grigoriy Dobryakov <grigoriydobryakov@gmail.com>
2015-01-03 01:40:38 +0300

    add file_url accessor

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,4 +3,10 @@ class Document < ActiveRecord::Base
   has_attached_file :file
   validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 
+  attr_reader :file_url
+
+  def file_url
+    self.file.url
+  end
+
 end
